200522810350a943ef5b8342fc98cbd554c7bf46
GamerBreadToaster <goedemoorgen@gmail.com>
2025-12-09 11:42:02 +0100

    Handle missing `save.json` file gracefully by initializing default data and adjust JSON formatting indentation

--- modules/info_getter.py
+++ modules/info_getter.py
@@ -2,8 +2,18 @@ from modules.players import Player, Settings
 import json
 
 def get_info():
-    with open("save.json", "r") as file:
-        return json.load(file)
+    try:
+        with open("save.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        with open("save.json", "w") as file:
+            data = {
+                "money": 2500,
+                "profit": 0,
+                "cooldown": 750
+            }
+            json.dump(data, file, indent=2)
+            return data
 
 def set_info(player: Player, settings: Settings):
     data = {
@@ -12,5 +22,5 @@ def set_info(player: Player, settings: Settings):
         "cooldown": settings.cooldown
     }
     with open("save.json", "w") as file:
-        json.dump(data, file, indent=4)
+        json.dump(data, file, indent=2)
         return None
